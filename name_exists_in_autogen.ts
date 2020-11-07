8b2f53417040e73d68f613c1a798b55e8ce26161
Jorge Cotillo <jcotillo@microsoft.com>
2020-11-06 21:45:55 -0800

    update code

--- name_exists_in_autogen.ts
+++ name_exists_in_autogen.ts
@@ -1,22 +1,11 @@
 import { executeSynchronous } from './utils';
-import fs from 'fs';
-import { promisify } from 'util';
-
-const writeFile = promisify(fs.writeFile);
 
 executeSynchronous(async () => {
     if (!process.argv[2]) {
         return;
     }
-
+    
     let prName = process.argv[2];
-    let regExp = /\[(.*?)\]/;
-    let matches = prName.match(regExp);
-    if (matches != null && matches.length > 0) {
-        let match = matches[1];
-        let split_match = match.split(" ");
-        if (split_match.length == 2) {
-            await writeFile("result.txt", split_match[1], { encoding: 'utf8' });
-        }
-    }
+    let basePath = prName.replace("jcotillo/", "");
+    console.log(`${basePath}/resource-manager`);
 });
