cd5045adecc9d9775b8ff2819b92f1e082f83a42
Jorge Cotillo <jcotillo@microsoft.com>
2020-11-06 21:51:24 -0800

    update worfklow

--- name_exists_in_autogen.ts
+++ name_exists_in_autogen.ts
@@ -6,6 +6,11 @@ executeSynchronous(async () => {
     }
     
     let prName = process.argv[2];
+
+    if (!prName.startsWith("jcotillo")) {
+        throw new Error("Failure!!! Branch name not valid");
+    }
+
     let basePath = prName.replace("jcotillo/", "");
     console.log(`${basePath}/resource-manager`);
 });
