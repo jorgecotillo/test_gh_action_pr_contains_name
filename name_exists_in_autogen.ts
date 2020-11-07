6339143b6f9275029eeac2b3f4de03f5e9d0ca17
Jorge Cotillo <jcotillo@microsoft.com>
2020-11-06 21:14:12 -0800

    final update

--- name_exists_in_autogen.ts
+++ name_exists_in_autogen.ts
@@ -1,23 +1,22 @@
 import { executeSynchronous } from './utils';
+import fs from 'fs';
+import { promisify } from 'util';
+
+const writeFile = promisify(fs.writeFile);
 
 executeSynchronous(async () => {
     if (!process.argv[2]) {
         return;
     }
 
-    console.log(process.argv[2]);
-
     let prName = process.argv[2];
     let regExp = /\[(.*?)\]/;
     let matches = prName.match(regExp);
     if (matches != null && matches.length > 0) {
-        console.log(matches[1]);
         let match = matches[1];
         let split_match = match.split(" ");
         if (split_match.length == 2) {
-            console.log(split_match[1]);
+            await writeFile("result.txt", split_match[1], { encoding: 'utf8' });
         }
     }
-
-    return true;
 });
